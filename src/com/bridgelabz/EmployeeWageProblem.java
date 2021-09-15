a1aa6823ac3acb1476d13b2c3d202b1a10a4090e
KumarB95 <kumarbalingal@gmail.com>
2021-09-15 12:50:44 +0530

    Modified : Calculating daily wages if Emp is present

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -6,12 +6,13 @@ public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage Problem");
         Random random = new Random();
-        int empCheck,IS_PRESENT=1,IS_ABSENT=0;
+        int empCheck,IS_PRESENT=1,IS_ABSENT=0,WAGE_PER_HOUR=20,FULL_DAY_HOUR=8,WagePerDay;
         empCheck=random.nextInt(2);
 
-        if (empCheck == IS_PRESENT)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is absent");
+        if (empCheck == IS_PRESENT){
+            WagePerDay=FULL_DAY_HOUR*WAGE_PER_HOUR;
+            System.out.println("Employee is Present and Wages per day : "+ WagePerDay+" Rs.");}
+        else{
+            System.out.println("Employee is absent wage for the day is Rs. 0");}
     }
 }
