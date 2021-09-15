630642037b8a184619022dcb61dbf00726ea695d
KumarB95 <kumarbalingal@gmail.com>
2021-09-15 12:42:54 +0530

    Modified : Checking wether Employee is present or absent using Random method

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage Problem");
+        Random random = new Random();
+        int empCheck,IS_PRESENT=1,IS_ABSENT=0;
+        empCheck=random.nextInt(2);
+
+        if (empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is absent");
     }
 }
